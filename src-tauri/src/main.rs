83d1cbd36bfc1d74961b71344f3e6adc8bdb17f6
screenfun <screenfun012@gmail.com>
2025-11-05 15:24:00 +0100

    fix: dodao tauri-plugin-updater u Cargo.toml i main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
